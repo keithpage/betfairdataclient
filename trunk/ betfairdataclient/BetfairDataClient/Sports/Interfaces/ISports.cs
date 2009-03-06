1d8c60e368d506e38e201b3fd58bea5285a2a321
dubdub1406 <dubdub1406@fdc37cb6-0a30-11de-b5bf-434079a3519f>
2009-03-06 12:47:22 +0000

    Added a basic ISports interface. Also added Views/MarketBase.cs and Views/EventBase.cs

---
+++ trunk/ betfairdataclient/BetfairDataClient/Sports/Interfaces/ISports.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Betfair.Data.Sports.Interfaces
+{
+    public interface ISports
+    {
+        /*** Events ***/
+
+        /// <summary>
+        /// Returns a list of Betfair events, if eventParentId value is set to 0 the root level events will be returned
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        List<Betfair.Data.Sports.Views.EventBase> GetEventTree(object[] args);
+
+        /*** Market Data Operations ***/
+
+        /// <summary>
+        /// Returns a single instance of the market
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        Betfair.Data.Sports.Views.MarketBase GetMarket(object[] args);
+
+        /// <summary>
+        /// This initiates the market object creation and adds the market to an internal watchlist
+        /// or in the case of push services it will subscribe to a market feed
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        Boolean RegisterMarket(object[] args);
+
+        /// <summary>
+        /// Remove the market from the watchlist
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        Boolean UnregisterMarket(object[] args);
+
+        /// <summary>
+        /// Returns true if the market is in the market watch list
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        Boolean MarketWatchListContains(object args);
+    }
+}
---
+++ trunk/ betfairdataclient/BetfairDataClient/Sports/Views/EventBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Betfair.Data.Sports.Views
+{
+    public sealed class EventBase
+    {
+        public EventBase() { }
+        public EventBase(object internalReferenceId)
+        {
+            this.internalReferenceId = internalReferenceId;
+        }
+
+        /// <summary>
+        /// The unique identifier for this object in this instance
+        /// </summary>
+        public  readonly object internalReferenceId;
+
+        /// <summary>
+        /// Data store for this element
+        /// </summary>
+        public Dictionary<object, object> values;
+
+        /// <summary>
+        /// The fine grained internal type of this objects values
+        /// eg. istypeof = Betfair.Data.Sports.Market.HorseRacing
+        /// or istypeof = Betfair.Data.Sports.Market.IrishTote
+        /// or istypeof = Betfair.Data.Sports.Market.AsianHandicap
+        /// </summary>
+        public readonly string isTypeOf;
+    }
+}
