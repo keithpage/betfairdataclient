54cb782370a4697541b5b981286780eba270ec76
dubdub1406 <dubdub1406@fdc37cb6-0a30-11de-b5bf-434079a3519f>
2009-03-07 11:09:01 +0000

    Fixed layout and ordering in preparation.

--- trunk/ betfairdataclient/BetfairDataClient/Sports/Views/EventBase.cs
+++ trunk/ betfairdataclient/BetfairDataClient/Sports/Views/EventBase.cs
@@ -1,4 +1,52 @@
-﻿using System;
+﻿/* 
+ * EventBase.cs
+ * 
+ * Copyright (c) 2009 © The Sporting Exchange Limited. All rights reserved. http://www.betfair.com.
+ * BETFAIR® and the BETFAIR LOGO are registered trade marks of The Sporting Exchange Limited. 
+ * Data on Betfair website(s) (including pricing data) is protected by © and database rights. 
+ * It may not be used for any purpose without a licence.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  	1. Redistributions of source code must retain the above copyright notice,
+ *		this list of conditions and the following disclaimer.
+ *
+ *	    2. Redistributions in binary form must reproduce the above copyright 
+ *		notice, this list of conditions and the following disclaimer in 
+ *		the documentation and/or other materials provided with the distribution.
+ *
+ *	    3. The names of the authors may not be used to endorse or promote products
+ *		derived from this software without specific prior written permission.
+ * 
+ * The BetfairDataClient project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The work is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * See the GNU General Public License for more details.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
+ * OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the "Lignite For Betfair Project".  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
